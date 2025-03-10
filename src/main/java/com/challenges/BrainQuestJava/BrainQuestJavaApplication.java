4725bb59ece1cc542c2f7faf84d3326a8b8798c6
anita <anita.zukowska2@gmail.com>
2025-03-10 14:27:32 +0100

    First Commit

---
+++ src/main/java/com/challenges/BrainQuestJava/BrainQuestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.challenges.BrainQuestJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainQuestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainQuestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenges/BrainQuestJava/BrainQuestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenges.BrainQuestJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainQuestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
