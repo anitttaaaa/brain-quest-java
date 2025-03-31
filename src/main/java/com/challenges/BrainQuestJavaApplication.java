69ff8246552542243cf18f7e650a9214c6ea2421
anita <anita.zukowska2@gmail.com>
2025-03-31 09:19:48 +0200

    Task16 added.

--- src/main/java/com/challenges/BrainQuestJavaApplication.java
+++ src/main/java/com/challenges/BrainQuestJavaApplication.java
@@ -4,6 +4,7 @@ import com.challenges.arrays.Task3;
 import com.challenges.arrays.Task4;
 import com.challenges.arrays.Task5;
 import com.challenges.basics.Task15;
+import com.challenges.basics.Task16;
 import com.challenges.ifElse.Task10;
 import com.challenges.inheritance.Task13;
 import com.challenges.inheritance.Task14;
@@ -40,7 +41,8 @@ public class BrainQuestJavaApplication {
 //		Task12.task12();
 //		Task13.task13();
 //		Task14.task14();
-		Task15.task15();
+//		Task15.task15();
+		Task16.task16();
 
 	}
 
